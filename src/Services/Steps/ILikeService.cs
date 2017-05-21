cb4cbc6a5c8311f2b2336d1b77d64f1dc51d7d0a
Igor Blinnikov <Igor.Blinnikov@gmail.com>
2017-05-21 20:34:15 +0300

    feat(like): introduce like service

---
+++ src/Services/Steps/ILikeService.cs
@@ -0,0 +1,7 @@
+namespace Blinnikov.Instouch.Services.Steps
+{
+    public interface ILikeService
+    {
+        void Like(string tag);
+    }
+}
---
+++ src/Services/Steps/LikeService.cs
@@ -0,0 +1,48 @@
+using System;
+using Blinnikov.Instouch.Services.Scrape;
+using OpenQA.Selenium;
+
+namespace Blinnikov.Instouch.Services.Steps
+{
+    public class LikeService : ILikeService
+    {
+        private readonly IWebDriver _driver;
+        private readonly IImageFetcher _fetch;
+        private readonly IElementFinder _elementFinder;
+
+        public LikeService(IWebDriver driver, IImageFetcher fetch, IElementFinder elementFinder)
+        {
+            this._driver = driver;
+            this._fetch = fetch;
+            this._elementFinder = elementFinder;
+        }
+
+        public void Like(string tag)
+        {
+            var images = this._fetch.ByTag(tag);
+            foreach(var image in images)
+            {
+                this._driver.Navigate().GoToUrl(image);
+                this.ProcessLike();
+            }
+        }
+
+        private void ProcessLike()
+        {
+            var likeButton = this._elementFinder.GetLikeButton();
+            var unlikeButton = this._elementFinder.GetUnlikeButton();
+
+            // Not liked
+            if(likeButton != null && unlikeButton == null) 
+            {
+                likeButton.Click();
+                Console.WriteLine("Liked");
+            }
+
+            if(likeButton == null && unlikeButton != null)
+            {
+                Console.WriteLine("Already liked");
+            }
+        }
+    }
+}
